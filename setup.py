79479ae10f4c6200bef4d0ad030175443c827641
Mashudu Molefe <Mashudu@explore-datascience.net>
2019-03-22 14:12:12 +0200

    another

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='mash',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url= 'https://github.com/mashd1997/mypackage',
+    url= 'https://github.com/mashd1997/mash',
     author='Mashudu Molefe',
     author_email='mashmolefe97@gmail.com'
 
